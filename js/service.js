cca6d2b00dece21c74607a295410d838efadf28d
Gustaverukau <118172262+Gustaverukau@users.noreply.github.com>
2025-03-06 09:15:23 +0100

    Add files via upload

---
+++ js/service.js
@@ -0,0 +1,43 @@
+
+let togg1= document.getElementById("togg1");
+let togg2= document.getElementById("togg2");
+let togg3= document.getElementById("togg3");
+
+let d1= document.getElementById("d1");
+let d2= document.getElementById("d2");
+let d3= document.getElementById("d3");
+
+
+togg1.addEventListener("click", ()=>{
+    if(getComputedStyle(d1).display != "none"){
+        d1.style.display = "none";
+    } else{
+        d1.style.display = "block";
+        d2.style.display = "none";
+        d3.style.display = "none";
+    }
+})
+
+togg2.addEventListener("click", ()=>{
+    if(getComputedStyle(d2).display != "none"){
+        d2.style.display = "none";
+    } else{
+        d1.style.display = "none";
+        d2.style.display = "block";
+        d3.style.display = "none";
+    }
+})
+togg3.addEventListener("click", ()=>{
+    if(getComputedStyle(d3).display != "none"){
+        d3.style.display = "none";
+    } else{
+        d1.style.display = "none";
+        d2.style.display = "none";
+        d3.style.display = "block";
+    }
+})
+
+
+
+
+
